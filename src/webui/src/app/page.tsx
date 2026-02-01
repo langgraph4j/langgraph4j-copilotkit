b44841c63e34711af9bb2d11bf468c40188cc64d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-02-01 18:15:22 +0100

    style(ui): update css

--- src/webui/src/app/page.tsx
+++ src/webui/src/app/page.tsx
@@ -6,7 +6,8 @@ import { SimpleChatWithApproval } from "./component/chatApproval";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SimpleChatWithApproval />
+      {/* <SimpleChatWithApproval /> */}
+      <SimpleChat />
     </main>
   );
 }
