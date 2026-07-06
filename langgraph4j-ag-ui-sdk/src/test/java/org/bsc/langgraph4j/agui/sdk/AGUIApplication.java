bc5ad2a4794c352248d495400133d8b7cf10d790
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-07-06 11:36:30 +0200

    test(langgraph4j-ag-ui-sdk): refine test application

--- langgraph4j-ag-ui-sdk/src/test/java/org/bsc/langgraph4j/agui/sdk/AGUIApplication.java
+++ langgraph4j-ag-ui-sdk/src/test/java/org/bsc/langgraph4j/agui/sdk/AGUIApplication.java
@@ -1,8 +1,5 @@
 package org.bsc.langgraph4j.agui.sdk;
 
-import com.agui.json.ObjectMapperFactory;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.StreamReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,16 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AGUIApplication {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        JsonFactory factory = JsonFactory.builder()
-        .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
-        .build();
-        var result = new ObjectMapper(factory);
-        ObjectMapperFactory.addMixins(result);
-        return result;
-    }
 
     @Bean
     AGUIAbstractLangGraphAgent createAgentExecutor(ObjectMapper objectMapper) {
