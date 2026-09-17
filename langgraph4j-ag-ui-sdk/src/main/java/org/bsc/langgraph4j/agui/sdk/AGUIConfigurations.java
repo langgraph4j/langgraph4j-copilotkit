6a3587f966171a0cca06d3ad4db299fb34ac5f37
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-09-17 19:47:42 +0200

    refactor: simplify AGUISerializer initialization

--- langgraph4j-ag-ui-sdk/src/main/java/org/bsc/langgraph4j/agui/sdk/AGUIConfigurations.java
+++ langgraph4j-ag-ui-sdk/src/main/java/org/bsc/langgraph4j/agui/sdk/AGUIConfigurations.java
@@ -15,16 +15,7 @@ public class AGUIConfigurations {
     @Bean
     public com.agui.community.core.serialization.Serializer aguiSerializer() {
 
-        JsonFactory factory = JsonFactory.builder()
-                .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
-                .build();
-
-        final var mapper = JsonMapper.builder(factory)
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .defaultPropertyInclusion(JsonInclude.Value.ALL_NON_NULL)
-                .build();
-
-        return new AGUIJacksonSerializer(mapper);
+        return new AGUIJacksonSerializer();
 
     }
 
