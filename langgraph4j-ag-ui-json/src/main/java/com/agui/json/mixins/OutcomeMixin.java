57d55f8aa1b562725cdce402087ecea8e55dc52a
bsorrentino <bartolomeo.sorrentino@gmail.com>
2026-09-19 23:52:03 +0200

    refactor: rename InterruptMixin to OutcomeMixin and update JsonSubTypes order

--- langgraph4j-ag-ui-json/src/main/java/com/agui/json/mixins/OutcomeMixin.java
+++ langgraph4j-ag-ui-json/src/main/java/com/agui/json/mixins/OutcomeMixin.java
@@ -2,18 +2,20 @@ package com.agui.json.mixins;
 
 import com.agui.community.core.interrupt.InterruptOutcome;
 import com.agui.community.core.interrupt.SuccessOutcome;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+/**
+ * Jackson mixin for the polymorphic {@code RunOutcome} hierarchy.
+ */
 @JsonTypeInfo(
         use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
         property = "type"
 )
-@JsonIgnoreProperties(value = "type", allowGetters = true)
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = InterruptOutcome.class, name = "interrupt"),
-        @JsonSubTypes.Type(value = SuccessOutcome.class, name = "success")
+        @JsonSubTypes.Type(value = SuccessOutcome.class, name = "success"),
+        @JsonSubTypes.Type(value = InterruptOutcome.class, name = "interrupt")
 })
-public class InterruptMixin {
+public interface OutcomeMixin {
 }
